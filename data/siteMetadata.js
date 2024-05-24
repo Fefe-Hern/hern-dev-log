69f1e6e250941e9b53fac4611137c2ef5100641d
FefeHern <fernando+git@hern.dev>
2024-05-24 22:24:07 +0000

    Blog

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'FefeHern',
+  title: 'Big Insights',
   author: 'Fernando Hernandez',
-  headerTitle: 'FefeHern',
-  description: 'Web Developer, BigCommerce, Miscellaneous Topics',
+  headerTitle: 'Big Insights',
+  description: 'Web Development, BigCommerce, Programming',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://www.hern.dev',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/Fefe-Hern/hern-dev-log',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   //mastodon: 'https://mastodon.social/@mastodonuser',
@@ -29,7 +29,7 @@ const siteMetadata = {
       // We use an env variable for this site to avoid other users cloning our analytics ID
       umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
       // You may also need to overwrite the script if you're storing data in the US - ex:
-      // src: 'https://us.umami.is/script.js'
+      src: 'https://cloud.umami.is/script.js',
       // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
     },
     // plausibleAnalytics: {
